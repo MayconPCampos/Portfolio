d70f5a3adf7c981b1ccd910fecd07981f48f91a4
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-25 14:47:16 -0300

    Nova fonte adicionada

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,9 +1,21 @@
 import { createGlobalStyle } from "styled-components";
+import font from "../fonts/Inter-Thin.ttf";
 
 const GlobalStyles = createGlobalStyle`
-  body{
-    background-color: #000;
-  }
+
+@font-face {
+  font-family: "Inter";
+  src: url(${font});
+}
+
+html{
+  font-family: "Inter";
+}
+
+body{
+  background-color: #000;
+}
+
 `;
 
 export default GlobalStyles;
