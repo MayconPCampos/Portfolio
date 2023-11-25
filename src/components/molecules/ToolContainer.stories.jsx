a0fefda14954329c083d5af416b6be3867c99c54
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-25 15:14:01 -0300

    Criado tool container stories

---
+++ src/components/molecules/ToolContainer.stories.jsx
@@ -0,0 +1,11 @@
+import ToolContainer from "./ToolContainer";
+
+import Title from "../atoms/Title";
+
+export default {
+  title: "Components/Molecules/ToolContainer",
+};
+
+export const Primary = {
+  render: () => <ToolContainer></ToolContainer>,
+};
