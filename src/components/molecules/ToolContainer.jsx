50ffb6488c7acf95d2dbfd9136e1b2e2e7403ed9
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-25 15:13:39 -0300

    Criado tool container component

---
+++ src/components/molecules/ToolContainer.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ToolContainer = styled.div`
+  border: 1px solid #fff;
+  width: 130px;
+  height: 200px;
+  border-radius: 80px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default ToolContainer;
