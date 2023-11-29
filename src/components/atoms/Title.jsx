ae10a5ec87c709d0884e53a19461bce87e688314
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-28 23:03:53 -0300

    Alterado tamanho da fonte dos titulos

--- src/components/atoms/Title.jsx
+++ src/components/atoms/Title.jsx
@@ -9,6 +9,30 @@ const Title = styled.div`
   h6 {
     color: #fff;
   }
+
+  h1 {
+    font-size: 4rem;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  h3 {
+    font-size: 2rem;
+  }
+
+  h4 {
+    font-size: 1.6rem;
+  }
+
+  h5 {
+    font-size: 1.4rem;
+  }
+
+  h6 {
+    font-size: 1.2rem;
+  }
 `;
 
 export const TitleColorSpan = styled.span`
