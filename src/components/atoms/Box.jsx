f30653bb2b219b835b74bfcf2ea977922158e680
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-28 22:50:29 -0300

    Criado novo component Box com tamanhos personalizados de containers

---
+++ src/components/atoms/Box.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const sizes = {
+  sm: {
+    width: "130px",
+    height: "200px",
+    borderRadius: "80px",
+  },
+  md: {
+    width: "300px",
+    height: "300px",
+    borderRadius: "30px",
+  },
+  lg: {
+    width: "600px",
+    height: "300px",
+    borderRadius: "30px",
+  },
+};
+
+export const BoxStyle = styled.div`
+  border: 1px solid #fff;
+  width: ${(props) => props.size.width};
+  height: ${(props) => props.size.height};
+  border-radius: ${(props) => props.size.borderRadius};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Box = ({ size }) => {
+  switch (size) {
+    case "lg":
+      return <BoxStyle size={sizes.lg}></BoxStyle>;
+    case "md":
+      return <BoxStyle size={sizes.md}></BoxStyle>;
+    default:
+      return <BoxStyle size={sizes.sm}></BoxStyle>;
+  }
+};
+
+export default Box;
