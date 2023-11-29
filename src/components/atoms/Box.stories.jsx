d67542d4c557f0892c68c871113fc2d523a4d91b
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-28 22:51:23 -0300

    Criado novo Box stories

---
+++ src/components/atoms/Box.stories.jsx
@@ -0,0 +1,17 @@
+import Box from "./Box";
+
+export default {
+  title: "Components/Atoms/Box",
+};
+
+export const Small = {
+  render: () => <Box size="sm"></Box>,
+};
+
+export const Medium = {
+  render: () => <Box size="md"></Box>,
+};
+
+export const Large = {
+  render: () => <Box size="lg"></Box>,
+};
