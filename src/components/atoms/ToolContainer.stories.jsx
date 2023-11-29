ad6c70d6b482c47d5d977e2e5d81ec2f10d308ed
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-28 22:52:18 -0300

    Alterado component ToolContainer para Box component e respectivo stories

--- src/components/atoms/ToolContainer.stories.jsx
+++
@@ -1,9 +0,0 @@
-import ToolContainer from "./ToolContainer";
-
-export default {
-  title: "Components/Molecules/ToolContainer",
-};
-
-export const Primary = {
-  render: () => <ToolContainer></ToolContainer>,
-};
