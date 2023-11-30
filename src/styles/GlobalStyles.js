c4f3ef86cb3c3de3f520f59bf68da058283c4243
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-11-30 18:12:00 -0300

    Novos pesos da fonte adicionados

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,11 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 import font from "../fonts/Inter-Thin.ttf";
+import fontRegular from "../fonts/Inter-Regular.ttf";
+import fontSemiBold from "../fonts/Inter-SemiBold.ttf";
 
 const GlobalStyles = createGlobalStyle`
 
 @font-face {
   font-family: "Inter";
   src: url(${font});
+}
+
+@font-face {
+  font-family: "Inter";
+  src: url(${fontRegular});
+  font-weight: 400;
+}
+
+@font-face {
+  font-family: "Inter";
+  src: url(${fontSemiBold});
+  font-weight: 800;
 }
 
 html{
