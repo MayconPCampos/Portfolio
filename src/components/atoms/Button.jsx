bea99a513a1a7d632c9ebff15e3b72e9b60ebea9
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-04 19:10:04 -0300

    Criado button component

---
+++ src/components/atoms/Button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = styled.button`
+  border-radius: 25px;
+  padding: 15px 25px;
+  color: #333;
+  background-color: #6eed8a;
+  border: none;
+  font-size: 1rem;
+  text-transform: uppercase;
+`;
+
+export default Button;
