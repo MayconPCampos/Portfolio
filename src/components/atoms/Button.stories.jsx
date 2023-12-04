3b6c6220b5ff8697e35865b0b5c4f66d24e2dd8e
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-04 19:10:31 -0300

    Criado button stories

---
+++ src/components/atoms/Button.stories.jsx
@@ -0,0 +1,10 @@
+import Button from "./Button";
+
+export default {
+  title: "Components/Atoms/Button",
+  component: Button,
+};
+
+export const Primary = {
+  render: () => <Button>primary</Button>,
+};
