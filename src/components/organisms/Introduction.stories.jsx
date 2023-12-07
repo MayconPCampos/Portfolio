8bbd298e707cfcb4041234998c07a0598746b41d
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-07 14:57:10 -0300

    Substituido Hero pelo Introduction component

---
+++ src/components/organisms/Introduction.stories.jsx
@@ -0,0 +1,10 @@
+import Introduction from "./Introduction";
+
+export default {
+  title: "Components/Organisms/Introduction",
+  component: Introduction,
+};
+
+export const Usage = {
+  render: () => <Introduction></Introduction>,
+};
