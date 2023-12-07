a86df2d9d1542a54958323097bfbbb2196815e6c
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-07 17:52:37 -0300

    Construida conteúdo de introdução da página

--- src/components/molecules/Introduction.jsx
+++ src/components/molecules/Introduction.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import Text from "../atoms/Text";
+import Title from "../atoms/Title";
+import Icon from "../atoms/Icon";
+
+import styled from "styled-components";
+import linkedinIcon from "../assets/linkedin.svg";
+import githubIcon from "../assets/github.svg";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  text-align: center;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  gap: 30px;
+`;
 
 const Introduction = () => {
-  return <div></div>;
+  return (
+    <Container>
+      <Text type="bold">Maycon P. Campos</Text>
+      <Title>
+        <h1>
+          Desenvolvedor <span>Front-end</span>
+        </h1>
+      </Title>
+      <Text type="bright">
+        Sou um programador<br></br>
+        com conhecimento de tecnologias para desenvolvimento front-end<br></br>
+        HTML, CSS, Javascript, React & React Native
+      </Text>
+      <IconContainer>
+        <Icon>
+          <img src={linkedinIcon} alt="" />
+        </Icon>
+        <Icon>
+          <img src={githubIcon} alt="" />
+        </Icon>
+      </IconContainer>
+    </Container>
+  );
 };
 
 export default Introduction;
