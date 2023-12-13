145821dca714434dfff7af5486cc21c2f7a21e13
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-13 18:57:45 -0300

    Criado skills component e respectivo stories

---
+++ src/components/Templates/Skills.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: #2c3e50;
+  width: 100%;
+  height: 600px;
+`;
+
+const Skills = () => {
+  return <Container></Container>;
+};
+
+export default Skills;
---
+++ src/components/Templates/Skills.stories.jsx
@@ -0,0 +1,10 @@
+import Skills from "./Skills";
+
+export default {
+  title: "Components/Templates/Skills",
+  component: Skills,
+};
+
+export const Usage = {
+  render: () => <Skills></Skills>,
+};
