7edc9d0c4ee16d8b4857e6d399f33d6242da7499
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-13 19:07:17 -0300

    Margem para tamanho md e menores

--- src/components/atoms/Section.jsx
+++ src/components/atoms/Section.jsx
@@ -2,6 +2,12 @@ import styled from "styled-components";
 import BreakpointSize, { BreakAt } from "../../styles/Breakpoints";
 
 const Section = styled.div`
+  margin: 2%;
+
+  ${BreakAt(BreakpointSize.md)} {
+    margin: 5%;
+  }
+
   ${BreakAt(BreakpointSize.lg)} {
     margin: 5%;
   }
