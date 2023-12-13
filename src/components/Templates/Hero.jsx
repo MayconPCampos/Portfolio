e7d1a284279f4f6796bfd854ddf8e474f9fa1020
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-13 19:16:57 -0300

    Novo tamanho de altura para tamanho menores que md

--- src/components/Templates/Hero.jsx
+++ src/components/Templates/Hero.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import styled from "styled-components";
 import bg from "../assets/bg.avif";
 import Introduction from "../molecules/Introduction";
+import BreakpointSize, { BreakAt } from "../../styles/Breakpoints";
 
 const Container = styled.div`
   background: url(${bg}), rgba(0, 0, 0, 0.6);
   background-blend-mode: overlay;
   background-position: center;
-  height: 100vh;
+  height: 60vh;
   display: flex;
   justify-content: center;
   align-items: center;
   padding: 0 10%;
+
+  ${BreakAt(BreakpointSize.md)} {
+    height: 100vh;
+  }
 `;
 
 const Hero = () => {
