5c87e9e11a593c592d5f007229385172928e2ff2
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-13 18:54:42 -0300

    Inserido component skills template

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Templates/Hero";
 import About from "../Templates/About";
+import Skills from "../Templates/Skills";
 
 const Index = () => {
   return (
     <>
       <Hero></Hero>
       <About></About>
+      <Skills></Skills>
     </>
   );
 };
