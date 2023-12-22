b59775f3f0a20d6f80a4d11cdd7f9695842305d7
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-22 14:42:54 -0300

    Inserido novos tamanhos de padding e width

--- src/components/atoms/Section.jsx
+++ src/components/atoms/Section.jsx
@@ -4,19 +4,28 @@ import BreakpointSize, { BreakAt } from "../../styles/Breakpoints";
 const Section = styled.div`
   padding: 2%;
   padding-top: 8%;
+  box-sizing: border-box;
+  display: flex;
+  margin: auto;
 
   ${BreakAt(BreakpointSize.md)} {
-    padding: 10%;
+    width: 650px;
+    padding: 0%;
+    padding-bottom: 100px;
   }
 
   ${BreakAt(BreakpointSize.lg)} {
-    padding: 10%;
+    width: 970px;
+    padding: 0%;
     padding-top: 5%;
+    padding-bottom: 150px;
   }
 
   ${BreakAt(BreakpointSize.xl)} {
-    padding: 15%;
+    width: 1200px;
+    padding: 0%;
     padding-top: 5%;
+    padding-bottom: 150px;
   }
 `;
 
