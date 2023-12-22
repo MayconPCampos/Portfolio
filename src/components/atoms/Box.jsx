169decf6fa1c28b73a0e452583c4803c66b3bd79
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-22 14:39:36 -0300

    Alterações de bordas e tamanhos

--- src/components/atoms/Box.jsx
+++ src/components/atoms/Box.jsx
@@ -3,32 +3,39 @@ import propTypes from "prop-types";
 
 const sizes = {
   sm: {
-    width: "130px",
-    height: "200px",
+    width: "110px",
+    height: "180px",
     borderRadius: "80px",
     borderColor: "#fff",
     padding: "25px",
+    borderTopRightRadius: "80px",
+    borderTopLeftRadius: "80px",
   },
   lg: {
-    width: "600px",
-    height: "1000px",
+    width: "700px",
+    height: "100%",
     borderRadius: "10px",
-    borderColor: "#bbb",
+    borderColor: "#eee",
     padding: "0px",
+    borderTopRightRadius: "0px",
+    borderTopLeftRadius: "0px",
   },
 };
 
 export const BoxStyle = styled.div`
-  border: 1px solid #bbb;
+  border: 1px solid transparent;
   border-color: ${(props) => props.size.borderColor};
   max-width: ${(props) => props.size.width};
   max-height: ${(props) => props.size.height};
   border-radius: ${(props) => props.size.borderRadius};
+  border-top-left-radius: ${(props) => props.size.borderTopLeftRadius};
+  border-top-right-radius: ${(props) => props.size.borderTopRightRadius};
   display: flex;
   row-gap: 20px;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  overflow: hidden;
   margin: auto;
   padding: ${(props) => props.size.padding};
 `;
