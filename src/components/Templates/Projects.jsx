f62927f758e6672a64123ca3b6ee0edf36d0e45e
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-22 14:49:19 -0300

    Alterado grid

--- src/components/Templates/Projects.jsx
+++ src/components/Templates/Projects.jsx
@@ -6,11 +6,15 @@ import projectImage from "../assets/project1.jpg";
 import styled from "styled-components";
 import Separator from "../atoms/Separator";
 import Card, { CardMedia, CardBody } from "../organisms/ProjectsCard";
+import Button from "../atoms/Button";
 
 const Container = styled.div`
   display: flex;
+  flex-wrap: wrap;
   flex-direction: column;
   align-items: center;
+  gap: 50px;
+  width: 100%;
 `;
 
 const Projects = () => {
@@ -21,17 +25,41 @@ const Projects = () => {
           <h2>Projetos</h2>
         </Title>
         <Separator />
-        <Grid sm={1} md={1} lg={2} xl={2} marginTop="5%" gap="50px">
-          <Card>
-            <CardMedia image={projectImage}></CardMedia>
-            <CardBody>
-              <Title>
-                <h4>
-                  Meu <span>site</span>
-                </h4>
-              </Title>
-            </CardBody>
-          </Card>
+        <Grid sm={1} md={2} lg={3} xl={3} marginTop="5%" gap="50px">
+          <div>
+            <Card>
+              <CardMedia image={projectImage}></CardMedia>
+              <CardBody>
+                <Button color="transparent">Ver no GitHub</Button>
+              </CardBody>
+            </Card>
+          </div>
+          <div>
+            <Card>
+              <CardMedia image={projectImage}></CardMedia>
+              <CardBody>
+                <Button color="transparent">Ver no GitHub</Button>
+              </CardBody>
+            </Card>
+          </div>
+          <div>
+            <Card>
+              <CardMedia image={projectImage}></CardMedia>
+              <CardBody>
+                <Button color="transparent">Ver no GitHub</Button>
+              </CardBody>
+            </Card>
+            <Button>Vizualizar</Button>
+          </div>
+          <div>
+            <Card>
+              <CardMedia image={projectImage}></CardMedia>
+              <CardBody>
+                <Button color="transparent">Ver no GitHub</Button>
+              </CardBody>
+            </Card>
+            <Button>Vizualizar</Button>
+          </div>
         </Grid>
       </Container>
     </Section>
