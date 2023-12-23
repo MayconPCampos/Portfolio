c3876960d8fd7d2f220b73329ce9c4719f4c361b
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-23 08:23:10 -0300

    Criado frame styled component para substituir o Box component

--- src/components/organisms/PerfilFrame.jsx
+++ src/components/organisms/PerfilFrame.jsx
@@ -1,21 +1,34 @@
 import React from "react";
 import styled from "styled-components";
 import Text from "../atoms/Text";
-import Box from "../atoms/Box";
+
 import perfilImage from "../assets/perfil.png.jpg";
 
 const Frame = styled.div`
-  width: 100%;
+  border: 1px solid #eee;
+  max-width: 700px;
+  max-height: 100%;
+  border-bottom-left-radius: 10px;
+  border-bottom-right-radius: 10px;
+  display: flex;
+  row-gap: 20px;
+  flex-direction: column;
+  align-items: start;
+  justify-content: center;
+  overflow: hidden;
+  margin: auto;
+  padding-bottom: 15px;
+
   img {
     max-width: 100%;
     margin-bottom: 20px;
   }
   p {
-    margin-left: 20px;
-    margin-right: 20px;
-    padding-bottom: 5px;
     border-bottom: 1px solid #eee;
     line-height: 1.5;
+    margin: 0 10px;
+    padding-bottom: 5px;
+    width: 100%;
   }
 
   p:nth-last-child(1) {
@@ -26,20 +39,18 @@ const Frame = styled.div`
 const PerfilFrame = () => {
   return (
     <>
-      <Box size="lg">
-        <Frame>
-          <img src={perfilImage} alt="" />
-          <Text>
-            <b>Nome:</b> Maycon Pinheiro Campos
-          </Text>
-          <Text>
-            <b>Residência:</b> Curitiba - PR
-          </Text>
-          <Text>
-            <b>Email:</b>mayconpinheirocampos@outlook.com
-          </Text>
-        </Frame>
-      </Box>
+      <Frame>
+        <img src={perfilImage} alt="" />
+        <Text>
+          <b>Nome:</b> Maycon Pinheiro Campos
+        </Text>
+        <Text>
+          <b>Residência:</b> Curitiba - PR
+        </Text>
+        <Text>
+          <b>Email:</b>mayconpinheirocampos@outlook.com
+        </Text>
+      </Frame>
     </>
   );
 };
