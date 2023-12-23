57a0aba178b51493a44d0bfa1f2cde36fddc2b26
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-23 11:48:30 -0300

    Criado objeto com as imagens

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -3,12 +3,19 @@ import About from "../Templates/About";
 import Skills from "../Templates/Skills";
 import Projects from "../Templates/Projects";
 
+const images = [
+  { key: 1, title: "React", url: "../assets/react.svg" },
+  { key: 2, title: "HTML5", url: "../assets/html5.svg" },
+  { key: 3, title: "CSS3", url: "../assets/css3-alt.svg" },
+  { key: 4, title: "Javascript", url: "../assets/js.svg" },
+];
+
 const Index = () => {
   return (
     <>
       <Hero></Hero>
       <About></About>
-      <Skills></Skills>
+      <Skills images={images}></Skills>
       <Projects></Projects>
     </>
   );
