17ff5d9ea065e8f7b63be9494f012d49f5274120
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-23 08:22:00 -0300

    Substituido component TechCard pelo component SkillCard

---
+++ src/components/organisms/SkillCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "styled-components";
+
+const Card = styled.div`
+  max-width: 110px;
+  max-height: 180px;
+  border-radius: 80px;
+  display: flex;
+  row-gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+  margin: auto;
+  padding: 25px;
+
+  img {
+    width: 100px;
+  }
+`;
+
+const SkillCard = ({ children }) => {
+  return <Card>{children}</Card>;
+};
+
+export default SkillCard;
---
+++ src/components/organisms/SkillCard.stories.jsx
@@ -0,0 +1,19 @@
+import Text from "../atoms/Text";
+import SkillCard from "./SkillCard";
+import reactImg from "../assets/react.svg";
+
+export default {
+  title: "components/Organisms/SkillCard",
+  component: SkillCard,
+};
+
+export const Usage = {
+  render: () => (
+    <>
+      <SkillCard>
+        <img src={reactImg}></img>
+        <Text type="bright">Tech</Text>
+      </SkillCard>
+    </>
+  ),
+};
