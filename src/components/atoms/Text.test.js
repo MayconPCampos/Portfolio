a424f5607be287d70689cbfad774b2deebf16ac4
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-24 14:56:47 -0300

    Alterado teste

--- src/components/atoms/Text.test.js
+++ src/components/atoms/Text.test.js
@@ -1,21 +1,20 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Text from "./Text";
 
 test("Renderiza Text com children", () => {
-  const view = render(<Text>teste</Text>);
+  render(<Text>teste</Text>);
 
   // eslint-disable-next-line testing-library/no-debugging-utils
-  view.debug();
+  screen.debug();
 
-  // eslint-disable-next-line testing-library/prefer-screen-queries
-  expect(view.getByText("teste")).toBeInTheDocument();
+  expect(screen.getByText("teste")).toBeInTheDocument();
 });
 
 test("Renderiza texto na cor especificada", () => {
-  const view = render(<Text type="number">12345</Text>);
+  render(<Text type="number">12345</Text>);
 
   // eslint-disable-next-line testing-library/prefer-screen-queries
-  expect(view.getByText("12345")).toHaveStyleRule({ color: "#68c3a3" });
+  expect(screen.getByText("12345")).toHaveStyleRule({ color: "#68c3a3" });
 });
--- src/components/atoms/Title.test.js
+++ src/components/atoms/Title.test.js
@@ -1,15 +1,14 @@
 import React from "react";
 
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 import Title from "./Title";
 
 test("Renderiza um texto na tela personalizado como titulo", () => {
-  const view = render(<Title>#titulo</Title>);
+  render(<Title>#titulo</Title>);
 
   // eslint-disable-next-line testing-library/no-debugging-utils
-  view.debug();
+  screen.debug();
 
-  // eslint-disable-next-line testing-library/prefer-screen-queries
-  expect(view.getByText("#titulo")).toBeInTheDocument();
+  expect(screen.getByText("#titulo")).toBeInTheDocument();
 });
