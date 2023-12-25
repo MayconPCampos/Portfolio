8eb8bd74668892db4cc5ae793b8fd76123341786
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-25 19:04:57 -0300

    Criadas listas de objetos

---
+++ src/data.js
@@ -0,0 +1,74 @@
+const projectsList = [
+  {
+    key: 1,
+    title: "Meu portifólio",
+    imageUrl: "./assets/portifolio.png",
+    gitHub: "https://github.com/MayconPCampos/Portfolio",
+    link: "#",
+  },
+  {
+    key: 2,
+    title: "Template The Photograph",
+    imageUrl: "./assets/photograph.png",
+    gitHub: "https://github.com/MayconPCampos/Template-responsivo-photograph",
+    link: "https://mayconpcampos.github.io/Template-responsivo-photograph/",
+  },
+  {
+    key: 3,
+    title: "Flask Website",
+    imageUrl: "./assets/flask-website.jpg",
+    gitHub: "https://github.com/MayconPCampos/Flask-Website",
+    link: "./assets/flask-website.jpg",
+  },
+  {
+    key: 4,
+    title: "Template Traveling blog",
+    imageUrl: "./assets/traveling-blog.png",
+    gitHub:
+      "https://github.com/MayconPCampos/Template-responsivo-traveling-blog",
+    link: "https://mayconpcampos.github.io/Template-responsivo-traveling-blog/",
+  },
+  {
+    key: 5,
+    title: "Kapple - em desenvolvimento",
+    imageUrl: "./assets/kapple-react.png",
+    gitHub: "https://github.com/MayconPCampos/Kapple",
+    link: "./assets/kapple-react.png",
+  },
+  {
+    key: 6,
+    title: "Kapple - js protótipo",
+    imageUrl: "./assets/kapple-prototipo.png",
+    gitHub: "https://github.com/MayconPCampos/Gerenciador-prototipo",
+    link: "https://mayconpcampos.github.io/Gerenciador-prototipo/",
+  },
+  {
+    key: 7,
+    title: "django Restfull api",
+    imageUrl: "./assets/chart-dashboard.jpg",
+    gitHub: "https://github.com/MayconPCampos/Django-Sales-Charts-RESTFul-API",
+    link: "./assets/chart-dashboard.jpg",
+  },
+  {
+    key: 8,
+    title: "flask api dashboard",
+    imageUrl: "./assets/flask-dashboard.jpg",
+    gitHub: "https://github.com/MayconPCampos/Flask-dashboard",
+    link: "./assets/flask-dashboard.jpg",
+  },
+  {
+    key: 9,
+    title: "docker - ambiente desenvolvimento",
+    imageUrl: "./assets/docker-image.png",
+    gitHub: "https://github.com/MayconPCampos/Ambiente-docker-flask",
+    link: "./assets/docker-image.png",
+  },
+];
+
+export const images = [
+  { key: 1, title: "React", url: "../assets/react.svg" },
+  { key: 2, title: "HTML5", url: "../assets/html5.svg" },
+  { key: 3, title: "CSS3", url: "../assets/css3-alt.svg" },
+  { key: 4, title: "Javascript", url: "../assets/js.svg" },
+];
+export default projectsList;
