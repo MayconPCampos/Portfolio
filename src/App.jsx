1bd7e0a5dface03d85337b5ca44c00ef435d3fdf
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-25 19:06:21 -0300

    Adicioonados components da página no app react

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Hero from "../src/components/Templates/Hero";
+import About from "../src/components/Templates/About";
+import Skills from "../src/components/Templates/Skills";
+import Projects from "../src/components/Templates/Projects";
+import Footer from "../src/components/Templates/Footer";
+import { images } from "./data";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <GlobalStyles />
+      <Hero />
+      <About />
+      <Skills images={images} />
+      <Projects />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
