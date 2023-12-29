2cdc859b4d3ca0acc01b39adcefe3c7459584a1e
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-29 15:32:04 -0300

    Criado stories com apresentação do storybook

---
+++ src/stories/Apresentacao.stories.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Title from "../components/atoms/Title";
+import styled from "styled-components";
+import Text from "../components/atoms/Text";
+
+const Container = styled.div`
+  margin: 50px;
+`;
+
+export default {
+  title: "Bem vindo(a)/Apresentação",
+};
+
+export const Portifolio = {
+  render: () => (
+    <Container>
+      <Title>
+        <h1>
+          <span>Seja bem vindo(a)</span>
+        </h1>
+      </Title>
+      <Text>
+        <p>
+          Este storybook foi criado para documentar visualmente o site com meu
+          portifólio.<br></br>Você pode acessar minha página através do link
+          {"  "}
+          <a href="#">meu site</a> para ver mais de meus projetos desenvolvidos
+          até o momento e aqueles que serão públicados no futuro.
+        </p>
+        <p>
+          Abaixo você encontra os componentes criados estruturados com Atomic
+          Design.
+        </p>
+        <p>Obrigado pela visita!</p>
+      </Text>
+    </Container>
+  ),
+};
