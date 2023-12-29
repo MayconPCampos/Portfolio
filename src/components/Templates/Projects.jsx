56df3f95ca6b3d1fb82d907dfef248df615fd632
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-29 15:58:44 -0300

    Inserido seção do storybook com cards

--- src/components/Templates/Projects.jsx
+++ src/components/Templates/Projects.jsx
@@ -9,7 +9,7 @@ import Icon from "../atoms/Icon";
 
 import siteLink from "../assets/visit-site.svg";
 import gitLink from "../assets/github.svg";
-import projectsList from "../../data";
+import projectsList, { Storybooks } from "../../data";
 
 const Container = styled.div`
   display: flex;
@@ -21,40 +21,71 @@ const Container = styled.div`
 
 const Projects = () => {
   return (
-    <Section>
-      <Container>
-        <Title>
-          <h2>Projetos</h2>
-        </Title>
-        <Separator />
-        <Grid sm={1} md={1} lg={2} xl={3} gap="50px">
-          {projectsList.map((project) => {
-            return (
-              <div key={project.key}>
-                <Card>
-                  <CardMedia image={project.imageUrl}></CardMedia>
-                  <CardBody>
-                    <a href={project.link} target="_blank">
-                      <Icon>
-                        <img src={siteLink} alt="" />
-                      </Icon>
-                    </a>
-                    <a href={project.gitHub} target="_blank">
-                      <Icon>
-                        <img src={gitLink} alt="" />
-                      </Icon>
-                    </a>
-                  </CardBody>
-                  <Title>
-                    <h5>{project.title}</h5>
-                  </Title>
-                </Card>
-              </div>
-            );
-          })}
-        </Grid>
-      </Container>
-    </Section>
+    <>
+      <Section>
+        <Container>
+          <Title>
+            <h2>Projetos</h2>
+          </Title>
+          <Separator />
+          <Grid sm={1} md={1} lg={2} xl={3} gap="50px">
+            {projectsList.map((project) => {
+              return (
+                <div key={project.key}>
+                  <Card>
+                    <CardMedia image={project.imageUrl}></CardMedia>
+                    <CardBody>
+                      <a href={project.link} target="_blank">
+                        <Icon>
+                          <img src={siteLink} alt="" />
+                        </Icon>
+                      </a>
+                      <a href={project.gitHub} target="_blank">
+                        <Icon>
+                          <img src={gitLink} alt="" />
+                        </Icon>
+                      </a>
+                    </CardBody>
+                    <Title>
+                      <h5>{project.title}</h5>
+                    </Title>
+                  </Card>
+                </div>
+              );
+            })}
+          </Grid>
+        </Container>
+      </Section>
+      <Section>
+        <Container>
+          <Title>
+            <h2>Storybooks</h2>
+          </Title>
+          <Separator />
+          <Grid lg={2} gap="50px">
+            {Storybooks.map((story) => {
+              return (
+                <div key={story.key}>
+                  <Card>
+                    <CardMedia image={story.imageUrl}></CardMedia>
+                    <CardBody>
+                      <a href={story.link} target="_blank">
+                        <Icon>
+                          <img src={siteLink} alt="" />
+                        </Icon>
+                      </a>
+                    </CardBody>
+                    <Title>
+                      <h5>{story.title}</h5>
+                    </Title>
+                  </Card>
+                </div>
+              );
+            })}
+          </Grid>
+        </Container>
+      </Section>
+    </>
   );
 };
 
