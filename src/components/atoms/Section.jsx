78d29023dfdba59eca75d786de937928469053e2
Maycon P Campos <mayconpinheirocampos@outlook.com>
2023-12-29 16:04:15 -0300

    Alterado padding

--- src/components/atoms/Section.jsx
+++ src/components/atoms/Section.jsx
@@ -11,21 +11,25 @@ const Section = styled.div`
   ${BreakAt(BreakpointSize.md)} {
     width: 650px;
     padding: 0%;
-    padding-bottom: 100px;
+    padding-bottom: 80px;
   }
 
   ${BreakAt(BreakpointSize.lg)} {
     width: 970px;
     padding: 0%;
-    padding-top: 5%;
-    padding-bottom: 150px;
+    padding-top: 80px;
+    padding-bottom: 80px;
   }
 
   ${BreakAt(BreakpointSize.xl)} {
     width: 1024px;
     padding: 0%;
-    padding-top: 5%;
-    padding-bottom: 150px;
+    padding-top: 100px;
+    padding-bottom: 100px;
+  }
+
+  &:nth-last-child(2) {
+    padding-top: 0;
   }
 `;
 
