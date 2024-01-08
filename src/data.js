5f640e8487443a14b548218666e28df7925f697c
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-07 22:03:34 -0300

    Alteração das informações e adicionada lista nova com skills

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 const projectsList = [
   {
     key: 1,
-    title: "Kapple web app - em desenvolvimento",
+    title: "Kapple web app - under development",
     imageUrl: "./assets/kapple-react.png",
     gitHub: "https://github.com/MayconPCampos/Kapple",
     link: "https://kapple.vercel.app/",
   },
   {
     key: 2,
-    title: "Meu portifolio",
+    title: "My portfolio page",
     imageUrl: "./assets/portifolio.png",
     gitHub: "https://github.com/MayconPCampos/Portfolio",
     link: "https://maycon-campos.vercel.app/",
@@ -16,14 +16,14 @@ const projectsList = [
 
   {
     key: 3,
-    title: "Template responsivo The Photograph",
+    title: "Responsive template The Photograph",
     imageUrl: "./assets/photograph.png",
     gitHub: "https://github.com/MayconPCampos/Template-responsivo-photograph",
     link: "https://mayconpcampos.github.io/Template-responsivo-photograph/",
   },
   {
     key: 4,
-    title: "Template responsivo Traveling blog",
+    title: "Responsive template Traveling blog",
     imageUrl: "./assets/traveling-blog.png",
     gitHub:
       "https://github.com/MayconPCampos/Template-responsivo-traveling-blog",
@@ -34,12 +34,12 @@ const projectsList = [
     title: "Flask Website",
     imageUrl: "./assets/flask-website.jpg",
     gitHub: "https://github.com/MayconPCampos/Flask-Website",
-    link: "./assets/flask-website.jpg",
+    link: "https://github.com/MayconPCampos/Flask-Website",
   },
 
   {
     key: 6,
-    title: "Protótipo kapple -  renderizado com js",
+    title: "Kapple prototype - js rendering",
     imageUrl: "./assets/kapple-prototipo.png",
     gitHub: "https://github.com/MayconPCampos/Gerenciador-prototipo",
     link: "https://mayconpcampos.github.io/Gerenciador-prototipo/",
@@ -49,45 +49,91 @@ const projectsList = [
     title: "django Restfull api",
     imageUrl: "./assets/chart-dashboard.jpg",
     gitHub: "https://github.com/MayconPCampos/Django-Sales-Charts-RESTFul-API",
-    link: "./assets/chart-dashboard.jpg",
+    link: "https://github.com/MayconPCampos/Django-Sales-Charts-RESTFul-API",
   },
   {
     key: 8,
     title: "flask stock news dashboard",
     imageUrl: "./assets/flask-dashboard.jpg",
     gitHub: "https://github.com/MayconPCampos/Flask-dashboard",
-    link: "./assets/flask-dashboard.jpg",
+    link: "https://github.com/MayconPCampos/Flask-dashboard",
   },
   {
     key: 9,
-    title: "docker - ambiente de desenvolvimento",
+    title: "Flask development environment image",
     imageUrl: "./assets/docker-image.png",
     gitHub: "https://github.com/MayconPCampos/Ambiente-docker-flask",
-    link: "./assets/docker-image.png",
+    link: "https://github.com/MayconPCampos/Ambiente-docker-flask",
   },
-];
-
-export const images = [
-  { key: 1, title: "React", url: "../assets/react.svg" },
-  { key: 2, title: "Storybook", url: "../assets/storybook.svg" },
-  { key: 3, title: "CSS3", url: "../assets/css3-alt.svg" },
-  { key: 4, title: "Javascript", url: "../assets/js.svg" },
 ];
 
 export const Storybooks = [
   {
     key: 1,
-    title: "Kapple web app",
+    title: "Kapple web app - under development",
     imageUrl: "./assets/storybook.png",
 
     link: "https://658f2e345a3535fab94e2971-gmqitohnry.chromatic.com/?path=/story/seja-bem-vindo-a--apresentacao",
   },
   {
     key: 2,
-    title: "Meu portifolio",
+    title: "My portfolio",
     imageUrl: "./assets/storybook.png",
 
     link: "https://65858e62b73aec278b43182e-luhshxwbtd.chromatic.com/?path=/story/bem-vindo-a-apresenta%C3%A7%C3%A3o--portifolio",
+  },
+];
+
+export const skills = [
+  {
+    key: 1,
+    title: "html",
+    progress: 95,
+  },
+  {
+    key: 2,
+    title: "css",
+    progress: 95,
+  },
+  {
+    key: 3,
+    title: "Javascript",
+    progress: 80,
+  },
+  {
+    key: 4,
+    title: "React",
+    progress: 80,
+  },
+  {
+    key: 5,
+    title: "Python",
+    progress: 90,
+  },
+  {
+    key: 6,
+    title: "Flask",
+    progress: 90,
+  },
+  {
+    key: 6,
+    title: "Django",
+    progress: 60,
+  },
+  {
+    key: 6,
+    title: "MySQL",
+    progress: 70,
+  },
+  {
+    key: 6,
+    title: "Java",
+    progress: 50,
+  },
+  {
+    key: 6,
+    title: "C++",
+    progress: 50,
   },
 ];
 
