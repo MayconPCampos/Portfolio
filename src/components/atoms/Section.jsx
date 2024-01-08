6bb455650d540795e51bef650923779fcbb7eecd
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-07 22:06:28 -0300

    Removido padding e display flex

--- src/components/atoms/Section.jsx
+++ src/components/atoms/Section.jsx
@@ -2,34 +2,22 @@ import styled from "styled-components";
 import BreakpointSize, { BreakAt } from "../../styles/Breakpoints";
 
 const Section = styled.div`
-  padding: 2%;
-  padding-top: 8%;
+  padding: 0% 2% 5%;
   box-sizing: border-box;
-  display: flex;
   margin: auto;
 
   ${BreakAt(BreakpointSize.md)} {
     width: 650px;
-    padding: 0%;
-    padding-bottom: 80px;
   }
 
   ${BreakAt(BreakpointSize.lg)} {
     width: 970px;
-    padding: 0%;
+
     padding-top: 80px;
-    padding-bottom: 80px;
   }
 
   ${BreakAt(BreakpointSize.xl)} {
-    width: 1024px;
-    padding: 0%;
-    padding-top: 100px;
-    padding-bottom: 100px;
-  }
-
-  &:nth-last-child(2) {
-    padding-top: 0;
+    width: 1280px;
   }
 `;
 
