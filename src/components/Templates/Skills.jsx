a351b54b0477b9232bbfd03fb475478511fe873f
Maycon P Campos <mayconpinheirocampos@outlook.com>
2024-01-07 22:13:47 -0300

    Alterada seção para mostrar barras de progresso ao invés das imagens e texto descritivo

--- src/components/Templates/Skills.jsx
+++ src/components/Templates/Skills.jsx
@@ -1,61 +1,49 @@
 import React from "react";
-import styled from "styled-components";
 import Section from "../atoms/Section";
 import Title from "../atoms/Title";
-import Text from "../atoms/Text";
-import SkillCard from "../organisms/SkillCard";
+import SkillBar from "../organisms/SkillBar";
 import Grid from "../atoms/Grid";
+import Separator from "../atoms/Separator";
+import styled from "styled-components";
 
-const Container = styled.div`
-  background-color: #2c3e50;
-  width: 100%;
-  height: fit-content;
-
-  h2 {
-    color: #fff;
-    margin-top: 5%;
-  }
-`;
-
-const TechInfo = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  margin-right: 15%;
-  margin-bottom: 5%;
-`;
-
-const Skills = ({ images }) => {
+const Skills = ({ skills }) => {
   return (
-    <Container>
+    <>
+      <Title id="skill-section">
+        <h2>Skills</h2>
+      </Title>
+      <Separator />
+
       <Section>
-        <Grid sm={1} md={1} lg={1} xl={2}>
-          <TechInfo>
-            <Title>
-              <h2>Tecnologias</h2>
-            </Title>
-            <Text type="bright">
-              Python, Flask, Django Framework, REST, APIs, entre outras são
-              algumas das ferramentas e conceitos com as quais já desenvolvi
-              pequenos projetos pessoais, atualmente tenho programado com React
-              e Storybook para documentar e testar diferentes idéias durante o
-              desenvolvimento.
-            </Text>
-          </TechInfo>
-          <Grid sm={2} md={4} lg={4} xl={4}>
-            {images.map((images) => {
-              return (
-                <SkillCard key={images.key}>
-                  <img src={images.url} alt="not" />
-                  <Text type="bright">{images.title}</Text>
-                </SkillCard>
-              );
-            })}
-          </Grid>
+        <Grid xl={2}>
+          {skills.map((data) => {
+            return <SkillBar skills={data}></SkillBar>;
+          })}
         </Grid>
       </Section>
-    </Container>
+    </>
   );
+};
+
+Skills.defaultProps = {
+  skills: [
+    {
+      title: "React",
+      progress: 100,
+    },
+    {
+      title: "HTML5",
+      progress: 100,
+    },
+    {
+      title: "CSS3",
+      progress: 100,
+    },
+    {
+      title: "Javascript",
+      progress: 100,
+    },
+  ],
 };
 
 export default Skills;
